35dc4855dd4fcafbec28b3ab92419b908ac3eee0
MiguelAtencio <49257405+MiguelAtencio@users.noreply.github.com>
2020-05-16 21:08:55 -0500

    Update Reglas.py

--- Proyecto/Reglas/Reglas.py
+++ Proyecto/Reglas/Reglas.py
@@ -9,9 +9,9 @@ import Tree.Tree as t
 import time
 
 
-Nf = 8
-Nc = 8
-No = 8
+Nf = 9
+Nc = 9
+No = 9
 
 letrasProposicionales = []
 for i in range(Nf):
@@ -21,12 +21,12 @@ for i in range(Nf):
             letrasProposicionales.append(chr(256 + v2))
 
 
-def subregla1NC(Nf,Nc,No,letras):
+def subregla1NC(Nf,Nc,No):
     regla1=""
     for k in range(0,Nf): #Nfilas->nacionalidad
         for j in range(0,Nc): #Ncols->atributos
             litt=[]
-            for i in letras:
+            for i in letrasProposicionales:
                 s,q,r=cf.decodifica3(ord(i)-256,Nf,Nc,No)
                 if s==k and q==j:
                     litt.append(i)
@@ -41,14 +41,40 @@ def subregla1NC(Nf,Nc,No,letras):
             regla1+=(len(litt)-1)*"O"
         regla1+=(Nc-1)*"Y"
     regla1+=(Nf-1)*"Y"
+    #print(regla1)
     return regla1
+
+def sub_regla2NC(Nf, Nc, No):
+    letrap=""
+    for k in range(0,Nf): #Nfilas->nacionalidad
+        for j in range(0,Nc): #Ncols->atributos
+            litt=[]
+            for i in letrasProposicionales:
+                s,q,r=cf.decodifica3(ord(i)-256,Nf,Nc,No)
+                if q==j and r==k:
+                    #print(s, q, r)
+                    litt.append(i)
+            for h in range(0,len(litt)):
+                ell=litt[h]
+                for t in range(0,len(litt)):
+                    if litt[t]==ell:
+                        letrap +=ell
+                    else:
+                        letrap+=litt[t]+'-'
+                letrap+=(len(litt)-1)*"Y"
+            letrap+=(len(litt)-1)*"O"
+        letrap+=(Nc-1)*"Y"
+    letrap+=(Nf-1)*"Y"
+    #print(letrap)
+    return letrap
+
 
 def sub_regla2(Nf,Nc,No):
     letrap = ''
-    for i in range(0, 8):
-        for h in range(0, 8):
-            for x in range(0, 8):
-                for j in range(0, 8):
+    for i in range(Nf):
+        for h in range(Nc):
+            for x in range(No):
+                for j in range(Nf):
                     if j == x:
                         a = cf.codifica3(j, i, h, Nf, Nc, No)
                         a = chr(a+256)
@@ -63,45 +89,30 @@ def sub_regla2(Nf,Nc,No):
     return letrap
 
 
-def sub_regla2Inorder(Nf,Nc,No):
-    letrap = ''
-    for i in range(0, 8):
-        for h in range(0, 8):
-            letrap += '('
-            for x in range(0, 8):
-                letrap += '('
-                for j in range(0, 8):
-                    if j == x:
-                        a = cf.codifica3(j, i, h, Nf, Nc, No)
-                        b = chr(a+256)
-                        letrap += '{}'.format(b)
-                    else:
-                        a = cf.codifica3(j, i, h, Nf, Nc, No)
-                        b = chr(a+256)
-                        letrap += '-{}'.format(b)
-                    if j != 7:
-                        letrap += 'Y'
-                if x != 7: letrap += ')O'
-                else: letrap += ')'
-            if h != 7: letrap += ')Y'
-            else: letrap += ')'
-    return letrap
 
-
-def regla_final():
-    a = ip.StringToTree(sub_regla1())
-    b = ip.StringToTree(sub_regla2())
-    return ip.Tree('Y', a, b)
+def regla_final(regla1, regla2):
+    a = t.StringToTree(regla1)
+    b = t.StringToTree(regla2)
+    return t.Tree('Y', a, b)
 
 
 if __name__ == '__main__':
-
+    
+    
     t1 = time.time()
-    R1=subregla1NC(Nf,Nc,No,letrasProposicionales)
-    tR1=t.StringToTree(R1)
-    TR1=str(tR1)
+
+    R1=subregla1NC(Nf,Nc,No)
+    #tR1=t.StringToTree(R1)
+    #TR1=str(tR1)
+    R2 = sub_regla2NC(Nf, Nc, No)
+    #tR2 = t.StringToTree(R2)
+    #TR2 = str(tR2)
+    RF = regla_final(R1, R2)
+
+    TRF = str(RF)
+    
     #se pasa a Tseitin para que genere una formula equisatisfacible mas sencilla que TR1
-    teis=fn.Tseitin(TR1,letrasProposicionales)
+    teis=fn.Tseitin(TRF,letrasProposicionales)
     #pasando a forma clausal
     hk=fn.formaClausal(teis)
     #se ingresa al DPLL
@@ -117,6 +128,8 @@ if __name__ == '__main__':
                 I2[j]=1
             else:
                 I2[j]=0
+
+    print(len(I2))
     print(I2)
     t2 = time.time()
     tf = t2 - t1
